bb206e2da72296472033c82116736b07f24f91bc
SergiGan <sj.shaynyuk@gmail.com>
2020-12-18 18:50:31 +0200

    Updates

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello AWS and Wellcome to my test lessons'));
+app.get('/', (req, res) => res.send('**_Hello AWS and Wellcome to my test lessons_**'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
