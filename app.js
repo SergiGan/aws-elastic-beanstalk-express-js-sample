bd5508cce5456ed7858b64945593bbd3de8c601a
SergiGan <sj.shaynyuk@gmail.com>
2021-01-25 00:58:10 +0200

    This full pipeline test

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require('express');   // conected to express
+// ------------- only node.js
+const http = require("http");
+const fs = require("fs");
+const port = 8081;  
+http.createServer(function(request, response){
+      
+    console.log("from url:", request.url);
+    // geting url from /
+    const filePath = "templates/" + request.url.substr(1);
 
-const app = express();                   // create object app
-
-const port = 3000;		           // define a handler for the "/" route
-
-app.use(express.static(__dirname + "/templates"));
- 
-app.use("/", (req, res) =>  // used static page
-     
-    res.send("index.html, help.html"));
- 
-app.listen(port);
-
-console.log("app running on: http://localhost:",port);
-
+    fs.readFile(filePath, function(error, data){
+              
+        if(error){
+                  
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }   
+        else{
+            response.end(data);
+        }
+    });
+}).listen(port, function(){
+    console.log(`Server started at : http://localhost:`, port, `at time`, Date.now("DD, HH, SS"));
+});
