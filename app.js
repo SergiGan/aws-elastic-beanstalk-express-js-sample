70e5b2a15613788739435dbacddfadce775e92a8
SergiGan <sj.shaynyuk@gmail.com>
2021-01-27 19:54:30 +0200

    change framew***

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-// ------------- only node.js
-const http = require("http");
-const fs = require("fs");
-const port = 8081;  
-http.createServer(function(request, response){
-      
-    console.log("from url:", request.url);
-    // geting url from /
-    const filePath = "templates/" + request.url.substr(1);
+const express = require('express');   // conected to express
 
-    fs.readFile(filePath, function(error, data){
-              
-        if(error){
-                  
-            response.statusCode = 404;
-            response.end("Resourse not found!");
-        }   
-        else{
-            response.end(data);
-        }
-    });
-}).listen(port, function(){
-    console.log(`Server started at : http://localhost:`, port, `at time`, Date.now("DD, HH, SS"));
-});
+const app = express();                   // create object app
+
+const port = 3000;		           // define a handler for the "/" route
+
+const today = new Date();
+const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+dateTime = date+' at '+time;
+
+app.use(express.static(__dirname + "/templates"));
+ 
+app.get("/templates", (req, res, next) =>  // used static page
+     
+    res.send("index.html, help.html"));
+ 
+app.listen(port);
+
+console.log(`App started on http://localhost:`, 'on', port, ',' , dateTime ); // ! or simple use full Date()
